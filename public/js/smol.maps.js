88ac81cb7cfa5d77a54969e8c6ff0920009f863c
Dan Phiffer <dan@phiffer.org>
2017-11-16 11:18:36 -0500

    :round_pushpin: hello map

--- public/js/smol.maps.js
+++ public/js/smol.maps.js
@@ -3,11 +3,40 @@ smol.maps = (function() {
 
 	var self = {
 
-		hello: function() {
-			return 'sup';
+		map: null,
+		config: null,
+
+		init: function() {
+			$.get('/api/config', function(config) {
+				self.config = config;
+				self.setup_map();
+			});
+		},
+
+		setup_map: function() {
+			self.map = L.map('map');
+			self.map.setView([42.753356, -73.681473], 16);
+			Tangram.leafletLayer({
+				scene: self.tangram_scene()
+			}).addTo(self.map);
+		},
+
+		tangram_scene: function() {
+			return {
+				global: {
+					sdk_mapzen_api_key: self.config.mapzen_api_key
+				},
+				import: [
+					"/scene/refill-style/refill-style.yaml"
+				]
+			};
 		}
 
 	};
+
+	$(document).ready(function() {
+		self.init();
+	});
 
 	return self;
 })();
