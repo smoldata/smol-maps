67b68cbcaeb4604a434c36707b4e9b90deb7dc7d
Dan Phiffer <dan@phiffer.org>
2017-11-19 11:43:13 -0500

    :mag: search for default location

--- public/js/smol.maps.js
+++ public/js/smol.maps.js
@@ -33,19 +33,83 @@ smol.maps = (function() {
 		setup_menu: function() {
 
 			$('#menu').on('config-setup', function() {
+				var place_click = function(e) {
+					var $place = $(e.target);
+					if (! $place.hasClass('place')) {
+						$place = $place.closest('.place');
+					}
+					$('#config-places .selected').removeClass('selected');
+					$place.addClass('selected');
+					var bbox = $place.data('bbox');
+					var wof_id = $place.data('wof-id');
+					$('input[name="default_bbox"]').val(bbox);
+					$('input[name="default_wof_id"]').val(wof_id);
+				};
+				$('#config-places-random').click(place_click);
+
+				var places_show = function(cb) {
+					var api_key = $('#config-api-key').val();
+					if (api_key == '') {
+						$('#config-location').attr('disabled', 'disabled');
+						$('#config-location').attr('placeholder', 'Search for a default location (requires API key)');
+					} else {
+						$('#config-location').attr('disabled', null);
+						$('#config-location').attr('placeholder', 'Search for a default location');
+					}
+					var text = $('#config-location').val();
+					if (text == '') {
+						$('#config-places-select').html('');
+						$('#config-places-random').trigger('click');
+					} else {
+						$.get('https://search.mapzen.com/v1/autocomplete?' + $.param({
+							text: text,
+							sources: 'wof',
+							api_key: api_key
+						})).then(function(rsp) {
+							var $select = $('#config-places-select');
+							var places = '';
+							$.each(rsp.features, function(i, feature) {
+								places += '<div class="place" data-bbox="' + feature.bbox.join(',') + '"data-wof-id="' + feature.properties.id + '"><span class="fa fa-check"></span> ' + feature.properties.label + '</div>'
+							});
+							$select.html(places);
+							$('#config-places-select .place').click(place_click);
+							if (typeof cb == 'function') {
+								cb(rsp);
+							}
+						});
+					}
+				};
+
 				$('#config-geolocate').click(function(e) {
 					e.preventDefault();
 					if ('geolocation' in navigator){
 						navigator.geolocation.getCurrentPosition(function(position) {
-							var lat = position.coords.latitude.toFixed(6);
-							var lng = position.coords.longitude.toFixed(6);
-							$('#config-latitude').val(lat);
-							$('#config-longitude').val(lng);
+							var api_key = $('#config-api-key').val();
+							if (api_key != '') {
+								$.get('https://search.mapzen.com/v1/reverse?' + $.param({
+									'point.lat': position.coords.latitude,
+									'point.lon': position.coords.longitude,
+									layers: 'locality',
+									sources: 'wof',
+									api_key: api_key
+								})).then(function(rsp) {
+									$('#config-location').val(rsp.features[0].properties.label);
+									places_show(function() {
+										$('#config-places-select .place:first-child').trigger('click');
+									});
+								});
+							}
 						});
 					} else {
 						alert('Your browser does not support geolocation.');
 					}
 				});
+
+				places_show();
+				$('#config-location').keypress(function(e) {
+					setTimeout(places_show, 0);
+				});
+				$('#config-api-key').change(places_show);
 			});
 
 			$('#menu').on('config-show', function() {
