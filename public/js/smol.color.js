73350dc1ccc3023275e7901a517783e7fafe1b4c
Dan Phiffer <dan@phiffer.org>
2017-11-22 10:17:06 -0500

    :rainbow: smol.color.js

---
+++ public/js/smol.color.js
@@ -0,0 +1,58 @@
+var smol = smol || {};
+smol.color = (function() {
+
+	var self = {
+
+		// from https://stackoverflow.com/a/5624139/937170
+		hex2rgb: function(hex) {
+			// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+			var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+			hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+				return r + r + g + g + b + b;
+			});
+
+			var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+			return result ? {
+				r: parseInt(result[1], 16),
+				g: parseInt(result[2], 16),
+				b: parseInt(result[3], 16)
+			} : null;
+		},
+
+		// from https://gist.github.com/mjackson/5311256
+		rgb2hsl: function(rgb) {
+			var r = rgb.r;
+			var g = rgb.g;
+			var b = rgb.b;
+			r /= 255, g /= 255, b /= 255;
+			var max = Math.max(r, g, b), min = Math.min(r, g, b);
+			var h, s, l = (max + min) / 2;
+			if (max == min) {
+				h = s = 0; // achromatic
+			} else {
+				var d = max - min;
+				s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+				switch (max) {
+					case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+					case g: h = (b - r) / d + 2; break;
+					case b: h = (r - g) / d + 4; break;
+				}
+
+				h /= 6;
+			}
+			return {
+				h: h,
+				s: s,
+				l: l
+			};
+		},
+
+		hex2hsl: function(hex) {
+			var rgb = self.hex2rgb(hex);
+			return self.rgb2hsl(rgb);
+		}
+	};
+
+	return self;
+})();
